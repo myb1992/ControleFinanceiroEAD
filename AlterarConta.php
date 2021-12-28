56c922093a3d620889eb3104d53aaece124aa76d
myb1992 <44007980+myb1992@users.noreply.github.com>
2021-12-28 11:04:50 -0300

    Create AlterarConta.php

---
+++ AlterarConta.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<?php
+include_once '_head.php';
+?>
+
+<body>
+<div id="wrapper">
+<?php
+include_once '_topo.php';
+include_once '_menu.php';
+?>
+<!--/. NAV SIDE -->
+<div id="page-wrapped">
+<div id="page-inner">
+<div class="row">
+<div class="col-md-12">
+<h2>Alterar Conta</h2>
+<h5>Aqui você poderá alterar ou excluir suas Contas</h5>
+</div>
+</div>
+<!--/. ROW -->
+<hr />
+<div class="form-group">
+<label> Nome do Banco*</label>
+<input class="form-control" placeholder="Digite o nome do Banco"</input>
+</div>
+<div class="form-group">
+<label> Agência*</label>
+<input class="form-control" placeholder="Digite a agência"</input>
+</div>
+<div class="form-group">
+<label> Número da conta*</label>
+<input class="form-control" placeholder="Digite o número da conta"</input>
+</div>
+<div class="form-group">
+<label>Saldo</label>
+<input class="form-control" placeholder="Digite o saldo da conta"</input>
+</div>  
+<button type="submit" class="btn btn-success">Salvar</button>
+<button type="submit" class="btn btn-danger">Excluir</button>
