b77329a0f8fadb5dbd425bc5cf3b133e2ed0f1c5
Matheus Seixas <44007980+myb1992@users.noreply.github.com>
2022-01-05 11:56:20 -0300

    Create pegar_dados.php

---
+++ pegar_dados.php
@@ -0,0 +1,41 @@
+<?php
+
+$nome = '';
+$sobre = '';
+
+if(isset($_POST['btn_mostrar'])){
+  $nome = $_POST['nome_usuario'];
+  $sobre = $_POST['sobrenome_usuario'];
+  
+  if(trim($nome) == ''){
+    echo 'Preencher o campo NOME';
+    }
+  else if(trim($sobre) ==''){
+    echo 'Preencher o campo SOBRENOME';
+}else{
+    
+  }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  
+  <body>
+    <form action="mostrar_dados.php" method="get">
+      <label>Nome</label>
+      <input name="nome_usuario" value="<?= $nome ?>">
+      <label>Sobrenome</label>
+      <input type="text" name="sobrenome_usuario"value="<?= $sobre ?>">
+      <button name="btn_mostrar">Mostrar</button>
+    </form>
+  </body>
+</html>
+
+    
