3d5f6ad7ff5204ccc6f9b0ef6ee2e4961914699c
Matheus Seixas <44007980+myb1992@users.noreply.github.com>
2022-02-03 15:14:43 -0300

    Update ex_conclusaodavenda.php

--- ex_conclusaodavenda.php
+++ ex_conclusaodavenda.php
@@ -1,27 +1,22 @@
-<?php
-    
+<?php   
     if(
         isset($_GET['nome_produto']) && $_GET['nome_produto'] != '' &&
         isset($_GET['valor_unitario']) && $_GET['valor_unitario'] != '' &&
         isset($_GET['quantidade']) && $_GET['quantidade'] != '' 
-      ){
-        
+      ){  
         $nome_pruduto = $_GET['nome_produto'];
         $valor_produto = $_GET['valor_unitario'];
         $quantidade_produto = $_GET['quantidade'];
-
 
         $conclusao = $valor_produto * $quantidade_produto;
          
          echo  "<b>Produto:</b> $nome_pruduto <br>
          <b>O valor unitario é de R$:</b> $valor_produto <br>
          <b>A quantidade foi:</b> $quantidade_produto <br>";
-
     }else{
         header('location:ex_venda.php');
         exit;
     }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,7 +27,6 @@
     <title>Document</title>
 </head>
 <body>
-    <label><b>Valor total R$ </b> <?= $conclusao?></label>
-    
+    <label><b>Valor total R$ </b> <?= $conclusao?></label>    
 </body>
 </html>
