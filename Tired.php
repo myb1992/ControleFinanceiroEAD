73b6783ca36c4adf0c2bd6643aad46cf7cb78ad4
Matheus Seixas <44007980+myb1992@users.noreply.github.com>
2022-03-08 14:23:00 -0300

    Create Tired.php

---
+++ Tired.php
@@ -0,0 +1,10 @@
+<?php
+
+if(happyness <= 5) {
+Echo 'Im so tired, I keep hoping that something will change with her, but it doesnt. This keeps draining all my energy. I dunno why she doesnt want me around...
+      Yet I really like her. All I want is to be close to her, but not how we are right now... I wish I could turn back time'
+else {
+Echo 'Its hard to change how we think, what we do during the day... I really wish we could be together Isa! I really do! I love you with all my heart! Id do anything for you!
+     Ive been through so much shit, yet the pain of not beeing with you, is stronger than anything I have ever faced.'
+  
+  ?>
